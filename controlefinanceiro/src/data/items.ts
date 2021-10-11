3e814ad1c9f9b9d64d686eb158c5568f074a091e
Gilberto Horacio <gilbertohoracio@gmail.com>
2021-10-11 16:26:02 -0300

    Project Controle Financeiro Created

---
+++ controlefinanceiro/src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 10, 9), category: 'food', title: 'Restaurante A mineira', value: 32.12 },
+    { date: new Date(2021, 10, 10), category: 'food', title: 'Restaurante Rei do Cozido', value: 28 },
+    { date: new Date(2021, 9, 20), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2021, 10, 5), category: 'salary', title: 'Salário ACME', value: 4500 },
+];
